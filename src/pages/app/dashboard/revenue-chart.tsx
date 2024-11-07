886de63533d5f43e98c4d11c788c554440186935
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-11-07 14:30:02 -0300

    refactor: upgrade chart

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,11 +1,4 @@
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  XAxis,
-  YAxis,
-} from 'recharts'
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import {
@@ -15,16 +8,29 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
 
 const chartData = [
-  { date: '10/12', revenue: 1200 },
-  { date: '11/12', revenue: 169 },
-  { date: '12/12', revenue: 1020 },
-  { date: '13/12', revenue: 560 },
-  { date: '14/12', revenue: 1970 },
-  { date: '15/12', revenue: 840 },
-  { date: '16/12', revenue: 670 },
+  { date: '10/11', revenue: 1200 },
+  { date: '11/11', revenue: 169 },
+  { date: '12/11', revenue: 1020 },
+  { date: '13/11', revenue: 560 },
+  { date: '14/11', revenue: 1970 },
+  { date: '15/11', revenue: 840 },
+  { date: '16/11', revenue: 670 },
 ]
+
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: colors.violet[500],
+  },
+} satisfies ChartConfig
 
 export function RevenueChart() {
   return (
@@ -38,9 +44,16 @@ export function RevenueChart() {
         </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={chartData} style={{ fontSize: 12 }}>
-            <XAxis dataKey={'date'} tickLine={false} axisLine={false} dy={16} />
+        <ChartContainer config={chartConfig} className="max-h-[240px] w-full">
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
 
             <YAxis
               stroke="#888"
@@ -54,17 +67,25 @@ export function RevenueChart() {
                 })
               }
             />
-
-            <CartesianGrid vertical={false} className="stroke-muted" />
-
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
             <Line
-              type="linear"
+              dataKey="revenue"
+              type="natural"
+              stroke="var(--color-desktop)"
               strokeWidth={2}
-              dataKey={'revenue'}
-              stroke={colors.violet[500]}
+              dot={false}
             />
           </LineChart>
-        </ResponsiveContainer>
+        </ChartContainer>
       </CardContent>
     </Card>
   )
