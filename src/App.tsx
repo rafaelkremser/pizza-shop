f34c878c7e3ca0e7642ce8d41a2f6d9188ced334
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-11-08 12:26:29 -0300

    feat: setup back-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -10,6 +12,7 @@ export function App() {
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
       <HelmetProvider>
         <Helmet titleTemplate="%s | pizza.shop" />
+        <QueryClientProvider client={queryClient} />
         <RouterProvider router={router} />
         <Toaster richColors />
       </HelmetProvider>
