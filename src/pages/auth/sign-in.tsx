b35c1b7287960999af8da694bfea23c935f7fcc7
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-11-12 11:57:39 -0300

    feat: connect with register and authenticate routes

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -16,18 +18,30 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    },
+  })
+
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
+    try {
+      await authenticate({ email: data.email })
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-
-    toast.info('Enviamos o link de autenticação para o seu e-mail.')
+      toast.info('Enviamos o link de autenticação para o seu e-mail.')
+    } catch {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
@@ -52,7 +66,7 @@ export function SignIn() {
 
             <div className="space-y-1">
               <Button disabled={isSubmitting} className="w-full" type="submit">
-                Finalizar cadastro
+                Acessar painel
               </Button>
 
               <p className="w-full text-center text-xs">
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -27,17 +29,24 @@ export function SignUp() {
 
   const navigate = useNavigate()
 
+  const { mutateAsync: registerAccount } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data)
-
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await registerAccount({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
 
       toast.success('Restaurante cadastrado!')
 
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
-      navigate('/sign-in')
+      navigate(`/sign-in?email=${data.email}`)
     } catch {
       toast.error('Erro ao cadastrar o restaurante.')
     }
